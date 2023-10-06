a14674684f49022de28e1de9fc6233ed59703e47
Michael Crowcroft <michaelcrowcroft@outlook.com>
2023-10-06 12:17:22 -0400

    setup repo for Laravel Package

---
+++ src/CloudIPServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MichaelCrowcroft\CloudIP;
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class CloudIPServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+      //
+    }
+
+    public function boot()
+    {
+      //
+    }
+  }
