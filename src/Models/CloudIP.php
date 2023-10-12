377bd61946b04b09f201bb56db6009e5828c5555
Michael Crowcroft <michaelcrowcroft@outlook.com>
2023-10-11 20:49:29 -0400

    update model

--- src/Models/CloudIP.php
+++ src/Models/CloudIP.php
@@ -4,7 +4,6 @@ namespace MichaelCrowcroft\CloudIP\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use MichaelCrowcroft\CloudIP\Database\Factories\CloudIPFactory;
 
 class CloudIP extends Model
 {
@@ -13,9 +12,4 @@ class CloudIP extends Model
     protected $table = 'cloud_ips';
 
     protected $guarded = [];
-
-    protected static function newFactory()
-    {
-        return CloudIPFactory::new();
-    }
 }
